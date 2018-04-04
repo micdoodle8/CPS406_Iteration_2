4fe19c06b36885cd3e9482d0116f352b01f24503
Niktib <nikolas.otto.maier@gmail.com>
2018-04-04 10:58:13 -0400

    Added a Method to Update DB
    Added a method to update the database one object at a time.
    Needs to be sent the string query as well as a linkedlist of values

--- app/src/main/java/ca/ryerson/scs/iteration2/ConnectToDatabase.java
+++ app/src/main/java/ca/ryerson/scs/iteration2/ConnectToDatabase.java
@@ -1,9 +1,11 @@
 package ca.ryerson.scs.iteration2;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.List;
 
 
 /**
@@ -55,5 +57,19 @@ public class ConnectToDatabase {
             ;
         }
         return rs;
+    }
+    //This function works by sending the query with ? where the values would be
+    //Utilise the setString method and a list of each value to edit the query
+    //As we should never need to mass update the database this one at a time method should work
+    public void AddData(String Query, List<String> Updates){
+        Integer i = 1;
+        try{
+            PreparedStatement pstmt = conn.prepareStatement(Query);
+            for (String s:Updates){
+                pstmt.setString(i, s);
+            }
+            pstmt.executeUpdate();
+        }catch (Exception e) {
+        }
     }
 }
