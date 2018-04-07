175b8650dd3f302a8b80088f7c5cff5327466e47
ykyng <37671753+ykyng@users.noreply.github.com>
2018-04-06 20:36:06 -0400

    Update TreasurerActivity.java

--- app/src/main/java/ca/ryerson/scs/iteration2/TreasurerActivity.java
+++ app/src/main/java/ca/ryerson/scs/iteration2/TreasurerActivity.java
@@ -21,5 +21,29 @@ public class TreasurerActivity extends AppCompatActivity {
             Intent openMembers = new Intent(TreasurerActivity.this, FinancesActivity.class);
             this.startActivity(openMembers);
         });
+        
+        findViewById(R.id.coachList).setOnClickListener( view -> {
+
+            Intent openCoaches = new Intent(TreasurerActivity.this, CoachListActivity.class);
+            TreasurerActivity.this.startActivity(openCoaches);
+        });
+
+        findViewById(R.id.setMeeting).setOnClickListener( view -> {
+
+            Intent openMeetingSetting = new Intent(TreasurerActivity.this, SetMeetingActivity.class);
+            TreasurerActivity.this.startActivity(openMeetingSetting);
+        });
+
+        findViewById(R.id.viewSchedule).setOnClickListener( view -> {
+            Intent openMembers = new Intent(TreasurerActivity.this, ViewScheduleActivity.class);
+            TreasurerActivity.this.startActivity(openMembers);
+        });
+
+        findViewById(R.id.memberList).setOnClickListener( view -> {
+            Intent openMembers = new Intent(TreasurerActivity.this, MembersActivity.class);
+            openMembers.putExtra("EXTRA_ISCOACH", true);
+            TreasurerActivity.this.startActivity(openMembers);
+        });
+
     }
 }
