e4bb3a8884ead0fd1e4c01c3cdfc5e4f5b2eaad0
Eugene <eugenemikhai@gmail.com>
2018-04-06 22:46:45 -0400

    updates

--- app/src/main/java/ca/ryerson/scs/iteration2/TreasurerActivity.java
+++ app/src/main/java/ca/ryerson/scs/iteration2/TreasurerActivity.java
@@ -11,13 +11,16 @@ public class TreasurerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treasurer);
 
-        /*
 
-        findViewById(R.id.treasurer_view_income).setOnClickListener( view -> {
+
+        findViewById(R.id.viewFinances).setOnClickListener( view -> {
 
             Intent openMeetingSetting = new Intent(TreasurerActivity.this, FinancesActivity.class);
             this.startActivity(openMeetingSetting);
         });
+
+
+        /*
 
         findViewById(R.id.treasurer_year_list).setOnClickListener( view -> {
             Intent openMembers = new Intent(TreasurerActivity.this, FinancesActivity.class);
